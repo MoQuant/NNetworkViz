47bd6ecf34fa81466895cc2534b521713a1fa10e
MoQuant <112667516+MoQuant@users.noreply.github.com>
2024-10-22 15:28:42 -0400

    Update nn.py

--- nn.py
+++ nn.py
@@ -2,53 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Net:
+class NeuralNetwork:
 
-    def __init__(self, inp, otp, epochs=100):
-        self.N = inp
-        self.M = otp
+    def __init__(self, iN, oN, epochs=50):
+        self.N = iN
+        self.M = oN
         self.epochs = epochs
 
     def __call__(self, plots, inputs, outputs):
         x, y = np.array(inputs), np.array(outputs)
         bias = -1
-
-        
         for epoch in range(self.epochs):
-
             for i in self.axis:
                 if i == self.axis[0]:
-                    self.layers[i] = inputs @ self.weights[i] + bias
+                    self.layers[i] = x @ self.weights[i] + bias
+                    self.slayers[i] = self.sigmoid(self.layers[i])
                 else:
                     self.layers[i] = self.layers[i+1] @ self.weights[i] + bias
-                self.slayers[i] = self.sigmoid(self.layers[i])
+                    self.slayers[i] = self.sigmoid(self.layers[i])
 
             for i in self.raxis:
                 if i == self.raxis[0]:
                     error = (y - self.slayers[i])**2
-                    delta = 2.0*(y - self.slayers[i])*self.sigmoid(self.layers[i], derv=True)
+                    delta = error*self.sigmoid(self.layers[i], derv=True)
                 else:
-                    #print(delta.shape, self.weights[i].shape, self.weights[i-1].shape)
                     error = self.weights[i-1] @ delta
                     delta = error * self.sigmoid(self.layers[i], derv=True)
-                    
 
-                self.weights[i] -= delta
+                self.weights[i] -= 3nn.0*delta
 
-            
-            #print("Number of Epochs left: ", self.epochs - epoch + 1)
+        for j, i in enumerate(self.axis):
+            z = self.weights[i]
+            mm, nn = z.shape
+            xx, yy = np.meshgrid(range(nn), range(mm))
+            plots[j].cla()
+            plots[j].set_title(f'Weights: {j + 1}')
+            plots[j].contourf(xx, yy, z, cmap='jet')
 
-        for i, j in enumerate(self.axis):
-            plots[i].cla()
-            plots[i].set_title(f'Weights: {i+1}')
-
-            z = self.weights[j]
-            m, n = z.shape
-            x, y = np.meshgrid(range(n), range(m))
-            plots[i].contourf(x, y, z, cmap='jet')
-
-        plt.pause(0.1)
-            
+        plt.pause(0.001)
 
     def sigmoid(self, x, derv=False):
         f = 1.0/(1.0 + np.exp(-x))
@@ -56,45 +47,34 @@ class Net:
             return f*(1 - f)
         return f
 
-    def buildWeights(self):
+    def buildParams(self):
         N, M = self.N, self.M
+        
+        self.axis = list(range(N, M, -1))
+        self.raxis = self.axis[::-1]
+
         self.weights = {}
         self.layers = {}
         self.slayers = {}
 
-        self.axis = list(range(N, M, -1))
-        self.raxis = self.axis[::-1]
-        
-        for t in self.axis:
-            self.weights[t] = np.random.rand(t, t-1)
-            self.layers[t] = np.zeros(t-1)
-            self.slayers[t] = np.zeros(t-1)
-
-    def normFunction(self, x, y):
-        x, y = np.array(x), np.array(y)
-        return (x - np.min(x))/(np.max(x) - np.min(x)), (y - np.min(y))/(np.max(y) - np.min(y))
+        for i in self.axis:
+            self.weights[i] = np.random.rand(i, i-1)
+            self.layers[i] = np.zeros(i - 1)
+            self.slayers[i] = np.zeros(i - 1)
 
 
-fig = plt.figure(figsize=(7, 7))
-ax = [fig.add_subplot(u) for u in (221, 222, 223, 224)]
+N = 300
+X = np.random.rand(N, 6)
+Y = np.random.rand(N, 2)
 
-n = 200
-m = 6
+net = NeuralNetwork(6, 2)
+net.buildParams()
 
-X = np.random.rand(n, m)
-Y = np.random.rand(n, 2)
+fig = plt.figure(figsize=(9, 8))
+plots = [fig.add_subplot(u) for u in (221,222,223,224)]
 
-net = Net(6, 2)
-net.buildWeights()
-
-nX, nY = net.normFunction(X, Y)
-
-for ii, (ix, iy) in enumerate(zip(X, Y)):
-    print("Rows left: ", n - ii + 1)
-    net(ax, ix, iy)
-
+for i, (ix, iy) in enumerate(zip(X, Y)):
+    print("rows left: ", N - i)
+    net(plots, ix, iy)
 
 plt.show()
-
-
-    
