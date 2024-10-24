6a159c11bb36259714860d2a41a9991f0b275ce2
MoQuant <112667516+MoQuant@users.noreply.github.com>
2024-10-23 22:41:50 -0400

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,56 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
+rcParams['figure.autolayout'] = True
 
-class NeuralNetwork:
+class AI:
 
-    def __init__(self, iN, oN, epochs=50):
-        self.N = iN
-        self.M = oN
-        self.epochs = epochs
+    def __init__(self, inputs, outputs, epochs=50):
+        self.M = inputs
+        self.N = outputs
+        self.epochs = range(epochs)
 
-    def __call__(self, plots, inputs, outputs):
-        x, y = np.array(inputs), np.array(outputs)
+    def __call__(self, ax, inx, outy):
+        x, y = np.array(inx), np.array(outy)
         bias = -1
-        for epoch in range(self.epochs):
+
+        for epoch in self.epochs:
             for i in self.axis:
                 if i == self.axis[0]:
                     self.layers[i] = x @ self.weights[i] + bias
                     self.slayers[i] = self.sigmoid(self.layers[i])
                 else:
-                    self.layers[i] = self.layers[i+1] @ self.weights[i] + bias
+                    self.layers[i] = self.slayers[i+1] @ self.weights[i] + bias
                     self.slayers[i] = self.sigmoid(self.layers[i])
 
+            dx = {}
             for i in self.raxis:
                 if i == self.raxis[0]:
                     error = (y - self.slayers[i])**2
-                    delta = error*self.sigmoid(self.layers[i], derv=True)
+                    delta = 2.0*(y - self.slayers[i])*self.sigmoid(self.layers[i], derv=True)
+                    dx[i] = delta
                 else:
                     error = self.weights[i-1] @ delta
                     delta = error * self.sigmoid(self.layers[i], derv=True)
+                    dx[i] = delta
 
-                self.weights[i] -= 3nn.0*delta
-
-        for j, i in enumerate(self.axis):
+            for i in self.axis:
+                self.weights[i] -= dx[i]
+            
+        for p, i in enumerate(self.axis):
             z = self.weights[i]
             mm, nn = z.shape
             xx, yy = np.meshgrid(range(nn), range(mm))
-            plots[j].cla()
-            plots[j].set_title(f'Weights: {j + 1}')
-            plots[j].contourf(xx, yy, z, cmap='jet')
+            ax[p].cla()
+            ax[p].set_title(f'Weights: {p+1}')
+            ax[p].contourf(xx, yy, z, cmap='viridis')
 
         plt.pause(0.001)
 
     def sigmoid(self, x, derv=False):
-        f = 1.0/(1.0 + np.exp(-x))
+        f = 1.0 / (1.0 + np.exp(-x))
         if derv:
             return f*(1 - f)
         return f
 
-    def buildParams(self):
-        N, M = self.N, self.M
-        
-        self.axis = list(range(N, M, -1))
+
+    def build(self):
+        M, N = self.M, self.N 
+        self.axis = list(range(M, N, -1))
         self.raxis = self.axis[::-1]
 
         self.weights = {}
@@ -58,23 +65,24 @@ class NeuralNetwork:
         self.slayers = {}
 
         for i in self.axis:
-            self.weights[i] = np.random.rand(i, i-1)
-            self.layers[i] = np.zeros(i - 1)
-            self.slayers[i] = np.zeros(i - 1)
+            self.weights[i] = 0.05*np.random.rand(i, i-1)
+            self.layers[i] = np.zeros(i-1)
+            self.slayers[i] = np.zeros(i-1)
 
 
-N = 300
-X = np.random.rand(N, 6)
-Y = np.random.rand(N, 2)
+fig = plt.figure(figsize=(10, 7))
+plots = [fig.add_subplot(2, 3, i) for i in range(1, 7)]
 
-net = NeuralNetwork(6, 2)
-net.buildParams()
+ai = AI(9, 3, epochs=100)
+ai.build()
 
-fig = plt.figure(figsize=(9, 8))
-plots = [fig.add_subplot(u) for u in (221,222,223,224)]
+rows = 200
+dataset = np.random.rand(rows, 9)
+output = np.random.rand(rows, 3)
 
-for i, (ix, iy) in enumerate(zip(X, Y)):
-    print("rows left: ", N - i)
-    net(plots, ix, iy)
+for index, (xx, yy) in enumerate(zip(dataset, output)):
+    print("Number of rows left: ", rows - index)
+    ai(plots, xx, yy)
+
 
 plt.show()
